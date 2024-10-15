403a201d2f8d6b0143d43430b9a4f7750fa0048c
Ciri1412 <charlesbugnalen08@gmail.com>
2024-10-15 17:43:25 +0800

    Odd or Even

---
+++ test.py
@@ -0,0 +1,6 @@
+print("Odd or Even Number")
+num = int(input("Enter a number to check if Odd or Even: "))
+if (num%2) == 0:
+    print(f"The number you input {num} is even number")
+else:
+    print(f"The number you input {num} is odd number")
